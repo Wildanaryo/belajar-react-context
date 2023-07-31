9ed8c758ba85efe8adfe9aae413383fab8ea5370
wildanaryo <wildanaryo@gmail.com>
2023-07-31 10:56:25 +0700

    delete unused code in app and main

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
-import { TYPES } from "./redux/types.js";
-import { onLogin } from "./redux/action/authAction";
-import axios, { Axios } from "axios";
-import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Users from "./pages/Users";
 import Login from "./pages/Login";
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { Provider } from "react-redux";
-import store from "./redux/store.js";
 import { AuthProvider } from "./Context/AuthProvider.jsx";
-import { ProductProvider } from "./Context/ProductProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <AuthProvider>
-        <ProductProvider>
-          <App />
-        </ProductProvider>
-      </AuthProvider>
-    </Provider>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
